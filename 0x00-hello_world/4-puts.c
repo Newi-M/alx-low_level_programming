8cc3d490db8cf6c9deddc78f8a8fdda0bca409a8
Newi-M <nawwal.moon77@gmail.com>
2022-12-09 01:03:05 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,13 @@
  * Created by: Nawwal
  * Dec 9, 2022
  */
-#include <studo.h>
-void main ()
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle"),
-	     return (0)
+	puts ("Programming is like building a multilingual puzzle");
+	     return (0);
 }
-
