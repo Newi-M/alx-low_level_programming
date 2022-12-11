8f264644e8fa2c406a3e23a7d5dfa955d7bb6aa8
Newi-M <nawwal.moon77@gmail.com>
2022-12-11 13:12:00 -0800

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers of base 16
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + 0);
+
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
