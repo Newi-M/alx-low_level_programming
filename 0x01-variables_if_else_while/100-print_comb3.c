e20c5639c058555f644805ba8928d6a38a23b6a2
Newi-M <nawwal.moon77@gmail.com>
2022-12-12 01:28:54 -0800

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 9; n1++)
+	{
+		for (n2 = 0; n2 < 9; n2++)
+		{
+			putchar((n1 % 10) + '0');
+			putchar((n2 % 10) + '0');
+			if (n1 == 8 && n2 == 9)
+			continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
