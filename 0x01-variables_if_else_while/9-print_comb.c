28648a20b2bacec763d43236f7dd51bba31c9da0
Newi-M <nawwal.moon77@gmail.com>
2022-12-12 01:14:14 -0800

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,17 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		putchar(n);
-		if (n != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	putchar('\n');
-	return (0);
+		putchar((n % 10) + '0');
+		if (n == 9)
+		continue;
+
+		putchar(',');
+		putchar(' ');
 	}
+
+	putchar('\n');
+
+	return (0);
 }
