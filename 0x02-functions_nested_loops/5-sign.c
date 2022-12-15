a76c6ba01b2fd73fa3c5385f01b8be8453f51dc8
Newi-M <nawwal.moon77@gmail.com>
2022-12-14 23:57:43 -0800

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,26 +2,26 @@
 
 /**
  * print_sign - Prints the sign of a number.
- * @n: The number of which the sign will be printed.
- * Return: 1 if the number is greater than zero,
- *         0 if the number is zero,
- *        -1 if the number is less than zero.
+ * @n: The input number as an integer.
+ * Return: 1 if the number is greater than zero, 0 if equals zero,
+ * -1 if less than zero.
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar('43');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar('48');
 		return (0);
 	}
 	else
 	{
-		_putchar('-');
+		_putchar('45');
 		return (-1);
 	}
+	_putchar('\n');
 }
