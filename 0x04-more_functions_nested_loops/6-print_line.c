191e0c602f36b5e50fee39380d8649ac98787b96
Newi-M <nawwal.moon77@gmail.com>
2022-12-15 04:22:24 -0800

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: takes in integer
+ * Return: returns nothing
+ */
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
