40ffcdecee11451b41883ff387333dc077f8c4f5
Newi-M <nawwal.moon77@gmail.com>
2022-12-29 06:46:21 -0800

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ *
+ * @size: input
+ *
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - i; j > 1; j--)
+			{
+				_putchar(32);
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
