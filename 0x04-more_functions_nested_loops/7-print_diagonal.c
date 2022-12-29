e885b0dfde3ff6bdfb8d23dec4b5b39da7924df5
Newi-M <nawwal.moon77@gmail.com>
2022-12-29 05:52:41 -0800

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ *
+ * @n: input number
+ *
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
