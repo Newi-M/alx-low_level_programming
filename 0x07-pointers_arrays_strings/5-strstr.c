e5ea415732a65b401acadcd700205a2bcd9664d5
Newi-M <nawwal.moon77@gmail.com>
2023-01-02 05:11:44 -0800

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - locates substring
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: substring or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a = 0, b = 0;
+
+	while (haystack[a])
+	{
+		while (needle[b])
+		{
+			if (haystack[a + b] != needle[b])
+			{
+				break;
+			}
+
+			b++;
+		}
+
+		if (needle[b] == '\0')
+		{
+			return (haystack + a);
+		}
+
+		a++;
+	}
+
+	return ('\0');
+}
