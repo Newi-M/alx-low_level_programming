fc20103e8eb2f40c38a38565908c3d5939b03940
Newi-M <118599845+Newi-M@users.noreply.github.com>
2023-01-05 20:45:52 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,22 +8,19 @@
 int main(void)
 {
 	int i = 0;
-	long j = 1, k = 2, sum;
+	long int j = 1, k = 2, sum;
 
-	while (i < 50)
+    printf("%d", j);
+    printf(", %d", k);
+
+	while (i < 48)
 	{
-		if (i == 0)
-			printf("%ld", j);
-		else if (i == 1)
-			printf(", %ld", k);
-		else
-		{
-			sum = j + k;
-			printf(", %ld", sum);
-			j = k;
-			k = sum;
-		}
-		i++;
+		sum = j + k;
+		printf(", %ld", sum);
+		j = k;
+		k = sum;
+		
+        i++;
 	}
 	printf("\n");
 	return (0);
