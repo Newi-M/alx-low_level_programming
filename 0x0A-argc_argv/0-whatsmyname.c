7cadbfcf3723d4f1eebe950c34fe547ce61f2c2b
Newi-M <nawwal.moon77@gmail.com>
2023-01-06 06:25:34 -0800

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: array of size argc
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
