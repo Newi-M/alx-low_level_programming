45e4e1b69a697c122f332cb8a29be9ecf1accb27
Newi-M <nawwal.moon77@gmail.com>
2023-01-11 06:32:55 -0800

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *p;
-	int i, j = 0, l1, l2, l;
+	int i, j, l;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -22,16 +22,14 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i] != 0; i++)
 	{
-		l1++;
+		;
 	}
-	for (i = 0; s2[i] != 0; i++)
+	for (l = 0; s2[l] != 0; l++)
 	{
-		l2++;
+		;
 	}
 
-	l = l1 + l2;
-
-	p = malloc((l + 1) * (sizeof(char)));
+	p = malloc((l + i + 1) * (sizeof(char)));
 
 	if (p == NULL)
 		return (NULL);
@@ -40,7 +38,10 @@ char *str_concat(char *s1, char *s2)
 	{
 		p[i] = s1[i];
 	}
-	for (i = l1; s2[j] != 0; i++)
+
+	j = 0;
+
+	for (; s2[j] != 0; i++)
 	{
 		p[i] = s2[j];
 		j++;
