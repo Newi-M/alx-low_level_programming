95089e48a391721ed305b8dad211738d4d36a625
Newi-M <nawwal.moon77@gmail.com>
2023-01-16 02:42:40 -0800

    0

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best");
+	return (0);
+}
---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: size
+ * @av: array
+ *
+ * Return: pointer or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+
