582a0b5f2ecc07148a80cc956f04e61e5684780f
Newi-M <nawwal.moon77@gmail.com>
2023-01-16 09:54:53 -0800

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+#include <stdlib.h>
+
+/**
+ * struct dog - struct for different elements
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
