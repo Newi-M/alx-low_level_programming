94bca301f85ec713e4686f732a948d825bd048dc
Newi-M <nawwal.moon77@gmail.com>
2023-01-17 05:31:34 -0800

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: dog
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
