0e874eda86b96e659261e2fa3ff662904adb8723
Newi-M <nawwal.moon77@gmail.com>
2023-01-18 05:08:07 -0800

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function
+ * @array: array
+ * @size: size of array
+ * @action: pointer
+ *
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || size == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(*(array + i));
+	}
+}
