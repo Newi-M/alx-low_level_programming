841997779d199ddfe8932b6da26d3729fa41954c
Newi-M <nawwal.moon77@gmail.com>
2023-01-18 04:30:46 -0800

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name
+ * @f: pointer
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
