a00c2172b8b1da405cd21d1537337f91fe876980
Newi-M <nawwal.moon77@gmail.com>
2023-01-18 07:02:06 -0800

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers"
+
+/**
+ * int_index - searches for an integer match
+ * @array: array
+ * @size: size
+ * @cmp: pointer
+ *
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i]))
+					return (i);
+			}
+		}
+	}
+
+	return (-1);
+}
