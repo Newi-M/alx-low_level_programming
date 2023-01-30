25d9451b4964ba057256cb844e98d6b8719d5fa9
Newi-M <nawwal.moon77@gmail.com>
2023-01-30 08:22:21 -0800

    3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,7 +17,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->next = *head;
+	new->next = NULL;
 	if (*head == NULL)
 	{
 		*head = new;
