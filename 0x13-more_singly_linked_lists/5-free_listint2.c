03823d8b0e4377d0a8afb91a3d0bd335f6463518
Newi-M <nawwal.moon77@gmail.com>
2023-01-30 13:02:26 -0800

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t **temp;
+	listint_t *temp;
 
 	while (*head != NULL)
 	{
-		*temp = (*head)->next;
+		temp = (*head)->next;
 		free(*head);
-		*head = *temp;
+		*head = temp;
 	}
+
+	head = NULL;
 }
