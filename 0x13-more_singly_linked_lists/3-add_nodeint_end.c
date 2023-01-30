05a022285818e174a7342d70154ac9635a8bee6b
Newi-M <nawwal.moon77@gmail.com>
2023-01-30 09:08:12 -0800

    3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,17 +5,15 @@
  * @head: linked list
  * @n: data
  *
- * Return:  the address of the new element, or NULL if it failed
+ * Return: address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
+
 	listint_t *last_before = *head;
 
 	new = (listint_t *)malloc(sizeof(listint_t));
-
-	if (new == NULL)
-		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	if (*last_before == NULL)
@@ -24,10 +22,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	else
 	{
-		while (last_before->next != NULL)
+		while ((last_before->next) != NULL)
+		{
 			last_before = last_before->next;
+		}
 		last_before->next = new;
 	}
 
-	return (*head);
+	return (new);
 }
