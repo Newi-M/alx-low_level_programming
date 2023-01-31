98eed9945afc96438daae9546ddbcdfff623be13
Newi-M <nawwal.moon77@gmail.com>
2023-01-31 05:06:59 -0800

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,15 +11,17 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	int i = 0;
 
-	if (head == NULL)
-		return (NULL);
-	for (; head != NULL; i++)
+	if (head != NULL)
 	{
-		if (i == index)
+		for (; head != NULL; i++)
 		{
-			return (head);
-		}
+			if (i == index)
+			{
+				return (head);
+			}
 
-		head = head->next;
+			head = head->next;
+		}
 	}
+	return (NULL);
 }
