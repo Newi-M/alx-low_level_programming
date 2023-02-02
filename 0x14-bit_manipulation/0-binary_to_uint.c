1484b4448adcfbab96dd0c39ede4c2935b3d3869
Newi-M <nawwal.moon77@gmail.com>
2023-02-02 04:52:30 -0800

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string of 0 & 1
+ *
+ * Return: converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, m, sum, len;
+	char *c;
+
+	len = _strlen(*b);
+	for (i = (len - 1); i >= 0; i--)
+	{
+		if (b[i] == 48 || b[i] == 49)
+		{
+			m = _pow(2, i);
+			c[i] = ((m) * b[i]);
+		}
+		else
+			return (0);
+	}
+	for (i = 0; c[i] != 0; i++)
+	{
+		sum += c[i];
+	}
+
+	return (sum);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -12,5 +12,46 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+unsigned int _strlen(char *c);
+unsigned int _pow(unsigned int i, unsigned int j);
+
+/**
+ * _strlen - length of string
+ * @c: string
+ *
+ * Return: length
+ */
+unsigned int _strlen(char *c)
+{
+	unsigned int len = 0;
+
+	while (*c != 0)
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * _pow - calculate power
+ * @i: base
+ * @j: exponent
+ *
+ * Return: answer
+ */
+unsigned int _pow(unsigned int i, unsigned int j)
+{
+	unsigned int product  = 1;
+
+	for (j; j > 0; j--)
+	{
+		product = product * i;
+	}
+	if (j == 0)
+		return (1);
+
+	return (product);
+}
 
 #endif
