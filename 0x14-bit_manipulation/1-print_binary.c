aa951231f868925be961833f6957d77a165d18cb
Newi-M <nawwal.moon77@gmail.com>
2023-02-02 06:54:45 -0800

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary number
+ * @n: decimal number
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar (n & 1 + '0');
+}
