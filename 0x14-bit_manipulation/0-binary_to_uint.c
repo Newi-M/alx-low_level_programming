f09a9caa2e795714876e857c43f45c007d252ec4
Newi-M <nawwal.moon77@gmail.com>
2023-02-02 06:41:21 -0800

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,16 +10,39 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int sum = 0;
 
+	int len = 0, base = 1;
+
+	if (!check_string(b))
+		return (0);
+
+	while (b[len] != '\0')
+		len++;
+
+	while (len)
+	{
+		sum += ((b[len - 1] - '0') * base);
+		base *= 2;
+		len--;
+	}
+	return (sum);
+}
+
+/**
+ * check_string - checks if a string has only 0's and 1's
+ * @b: string to be checked
+ *
+ * Return: 1 if string is valid, 0 otherwise
+ */
+int check_string(const char *b)
+{
+	if (b == NULL)
+		return (0);
+
 	while (*b)
 	{
-		if (*b == '1')
-			sum = ((sum << 1) + 1) | 1;
-		else if (*b == '0')
-			sum <<= 1;
-		else
+		if (*b != '1' && *b != '0')
 			return (0);
 		b++;
 	}
-
-	return (sum);
+	return (1);
 }
