74351802e5ebc3abaee1a4e5a947a8e2d29fdc76
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2023-02-04 22:22:52 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - print fibonacci numbers
+ * main - prints fibonacci numbers
  *
- * Return: 0
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	int i = 0;
+	int num = 0;
 
-	long int j = 1, k = 2, sum;
+	long int num1 = 1, num2 = 2, sum;
 
-	printf("%ld", j);
-	printf(", %ld", k);
+	printf("%ld", num1);
+	printf(", %ld", num2);
 
-	while (i < 48)
+	while (num < 48)
 	{
-		sum = j + k;
+		sum = num1 + num2;
 		printf(", %ld", sum);
-		j = k;
-		k = sum;
+		num2 = num1;
+		num1 = sum;
 
-		i++;
+		num++;
 	}
 
 	printf("\n");
