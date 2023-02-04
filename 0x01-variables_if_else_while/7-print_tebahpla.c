8ca9b06878098d93ea72a921c28f144da369e657
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2023-02-04 20:15:28 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - print lower case in reverse
+ * main - prints lower case characters in reverse
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
