4526af8c3811a1ce9bb40d30d6c6fc912c5ca7ed
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2023-02-04 21:08:08 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,9 +2,9 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
+ * @a: first integer number
+ * @b: second integer number
+ * @c: third integer number
  * Return: largest number
  */
 
