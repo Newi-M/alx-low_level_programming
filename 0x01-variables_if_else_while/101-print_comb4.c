19cf75873b113a59d6c70db04933fdf694515d7c
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2023-02-04 20:23:53 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry
+ * main - prints all possible different combinations of three digits
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	int n, m, k;
+	int num1, num2, num3;
 
-	for (n = '0'; n <= '9'; n++)
+	for (num1 = '0'; num1 <= '9'; num1++)
 	{
-		for (m = n + 1; m <= '9'; m++)
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
 		{
-			for (k = m + 1; k <= '9'; k++)
+			for (num3 = num2 + 1; num3 <= '9'; num3++)
 			{
-				putchar(n);
-				putchar(m);
-				putchar(k);
-				if (n != '7' || m != '8' || k != '9')
+				putchar(num1);
+				putchar(num2);
+				putchar(num3);
+				if (num1 != '7' || num2 != '8' || num3 != '9')
 				{
 					putchar(',');
 					putchar(' ');
