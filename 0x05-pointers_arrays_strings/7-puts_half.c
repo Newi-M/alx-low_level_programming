54761091c20c263f6494c80234397b538439a254
Newi-M <nawwal.moon77@gmail.com>
2023-03-17 12:29:02 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,21 +6,22 @@
 */
 void puts_half(char *str)
 {
-  int len = _strlen(str);
-  if (len % 2 == 0)
-  {
-    len = len / 2;
-  }
-  else
-  {
-    len = (len + 1) / 2;
-  }
-  while (str[len] != '\0')
-  {
-    _putchar(str[len]);
-    len++;
-  }
-  _putchar('\n');
+	int len = _strlen(str);
+
+	if (len % 2 == 0)
+	{
+		len = len / 2;
+	}
+	else
+	{
+		len = (len + 1) / 2;
+	}
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
 }
 
 
@@ -31,11 +32,13 @@ void puts_half(char *str)
  */
 int _strlen(char *s)
 {
-  int count, inc;
-  inc = 0;
-  for (count = 0; s[count] != '\0'; count++)
-  {
-    inc++;
-  }
-  return (inc);
+	int count, inc;
+
+	inc = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		inc++;
+	}
+	return (inc);
 }
