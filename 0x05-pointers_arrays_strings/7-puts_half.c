de9320ac5a2d49c9951b535de39d298d3235feca
Newi-M <nawwal.moon77@gmail.com>
2023-12-21 01:45:15 -0800

    7-puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints the last half of a string
+ *
+ * @str: string
+ *
+ * Return - none
+ */
+
+void puts_half(char *str)
+{
+	int len, i;
+
+	len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		len = len / 2;
+	}
+	else
+	{
+		len = (len + 1) / 2;
+	}
+
+	for (; str[len] != '\0'; len++)
+	{
+		_putchar(str[len]);
+	}
+
+	_putchar('\n');
+}
